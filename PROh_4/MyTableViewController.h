3eeb6c8651268c065a90164eec8b97459ba6246d
Andrii <tieorange@gmail.com>
2015-10-28 15:08:46 +0300

    myTableView added

---
+++ PROh_4/MyTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  MyTableViewController.h
+//  PROh_4
+//
+//  Created by Admin on 10/28/15.
+//  Copyright © 2015 tieorange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+@interface MyTableViewController : UITableViewController
+
+@end
+
+
